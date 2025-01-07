f76eb8eb47b6bbaf63afd935692a2386870264b8
NitinTSutar <nitintsuthar67@gmail.com>
2025-01-08 01:45:24 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let imageCounter = 11; // Start from 5 since 1 to 4 are already used
+let imageCounter = 11; 
 
 const swiper = new Swiper('.slider-wrapper', {
     loop: false,
@@ -31,11 +31,10 @@ const swiper = new Swiper('.slider-wrapper', {
 });
 
 const addNewCard = () => {
-    // Create a new card-item div
+
     const newCard = document.createElement('div');
     newCard.className = 'card-item swiper-slide';
 
-    // Add image to the card with incremented random number
     const img = document.createElement('img');
     img.src = `https://picsum.photos/600/280?random=${imageCounter++}`;
     img.alt = 'Image';
@@ -43,15 +42,12 @@ const addNewCard = () => {
 
     newCard.appendChild(img);
 
-    // Append the new card to the card-list
     const cardList = document.querySelector('.card-list');
     cardList.appendChild(newCard);
 
-    // Update Swiper
     swiper.update();
 };
 
-// Add event listener to the next button
 document.querySelector('.swiper-button-next').addEventListener('click', addNewCard);
 
 setTimeout(function() {
